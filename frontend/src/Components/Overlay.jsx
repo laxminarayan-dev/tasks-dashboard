c129102251c9a68b1dc757b7c9c7837aabb1d042
laxminarayan-dev <jaiswallucky0112@gmail.com>
2026-01-29 08:39:59 +0530

    Main content shift by sidebar opening fixed

--- frontend/src/Components/Overlay.jsx
+++ frontend/src/Components/Overlay.jsx
@@ -9,7 +9,10 @@ const Overlay = ({ isSidebarOpen, handleSidebarToggle }) => {
             className="fixed inset-0 bg-black opacity-50 backdrop-blur-2xl z-30 md:hidden"
             onClick={handleSidebarToggle}
           ></div>
-          <button onClick={handleSidebarToggle} className="md:hidden">
+          <button
+            onClick={handleSidebarToggle}
+            className="fixed z-100 md:hidden"
+          >
             <RxCross2 className="fixed top-6 right-8 text-slate-100 text-2xl cursor-pointer z-40 md:hidden" />
           </button>
         </Fragment>
